66f0a2e45b162643cc6edc05e450873b88f781f8
NitrogenDioxide <45130240+Kiffolisk@users.noreply.github.com>
2021-11-14 22:54:06 +0100

    Update ls.py

--- ls.py
+++ ls.py
@@ -2,7 +2,7 @@ import os
 
 def run(setpathfunc, osdir, username, curpath, args):
     for path in os.listdir(curpath):
-        if os.path.isfile(path):
+        if os.path.isfile(path) == True:
             print("[FILE] " + str(path))
-        elif os.path.isdir(path):
+        if os.path.isdir(path) == True:
             print("[DIR] " + str(path))
