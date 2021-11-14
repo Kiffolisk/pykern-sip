fa5939c31ed0497bdc47f68888444aacf1be6a11
NitrogenDioxide <45130240+Kiffolisk@users.noreply.github.com>
2021-11-14 23:02:27 +0100

    Update ls.py

--- ls.py
+++ ls.py
@@ -3,7 +3,7 @@ import os
 def run(setpathfunc, osdir, username, curpath, args):
     for path in os.listdir(curpath):
         # i hate this
-        if os.path.isfile(curpath + path) == True:
+        if os.path.isfile(curpath + "/" + path) == True:
             print("[FILE] " + str(path))
-        if os.path.isdir(curpath + path) == True:
+        if os.path.isdir(curpath + "/" + path) == True:
             print("[DIR] " + str(path))
