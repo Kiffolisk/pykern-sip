4006074a8f5cbc6529303c799fec6324a964fea1
NitrogenDioxide <45130240+Kiffolisk@users.noreply.github.com>
2021-11-16 18:03:46 +0100

    Update cd.py

--- cd.py
+++ cd.py
@@ -1,8 +1,11 @@
 import os
 
 def run(funcs, ivars):
+    setpathfunc = funcs[0]
     args = ivars[3]
     curpath = ivars[2]
+    osdir = ivars[0]
+    username = ivars[1]
     if len(args) != 2:
         print("Usage:")
         print("\tcd [dir]")
